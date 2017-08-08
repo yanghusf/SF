1496a83223feb3bf779f5c4981d997c3f5807dad
yanghusf <822028465@qq.com>
2017-08-08 16:58:13 +0800

    version 1.0

---
+++ Workspace/blogproject/blog/models.py
@@ -0,0 +1,23 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag,blank=True)
+    author = models.ForeignKey(User)
